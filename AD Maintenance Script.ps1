56e061264e6243a6c43ab02f914210a95bf6f9de
JSnow38 <106977147+JSnow38@users.noreply.github.com>
2022-06-08 11:46:37 -0400

    Add files via upload
    This script will perform the following:
    
    1. Verifies the existence or Build a Disabled Users and Disabled Computers OU
    
    2. Target your User/Computer OUs and scan for accounts that have not logged on within a set amount of time (default value is 30 days), disable them, move them to Disabled OU, and adds a description on the account of where it was moved from (for when it needs to be re-enabled and moved back to the original OU).
    
    3.Target your Disabled User/Computer OUs and scan for accounts that have not been used within a set amount of time (default value is 60 days), create a CSV File to record them, and then delete them from AD
    
    ###Some parameters must be modified and are explained at the headline of the PS1 file###
    
    Disclaimer: Use at your own risk.

---
+++ AD Maintenance Script.ps1
@@ -0,0 +1,116 @@
+﻿###Modify values of lines 4,7-9,12,15,18,and 21 for your network setup#####
+
+#Domain/parent OU#
+$MainOU= 'DC=adatum,DC=com'
+
+#OU we will target to scan for accounts and devices that are not in compliance#
+$TargetOU= @( 'OU=IT,DC=adatum,dc=com'
+                'OU=Computers,DC-Adatum,dc=com'
+                )
+ 
+#OU for Disabled Users# 
+$DisabledUsersOU = "OU=Disabled Users,DC=Adatum,DC=com"
+ 
+#OU for Disabled Computers# 
+$DisabledComputersOU = "OU=Disabled Computers,DC=Adatum,DC=com"
+ 
+#Inactivity threshhold for Disabled - 30 days in my example# 
+$InactivityDisable = "30.00:00:00"
+
+#Inactivity threshhold for Delete - 60 days in my example# 
+$InactivityDelete = "60.00:00:00"
+
+###Verifying and/or Building the Disabled OUs###
+if (Get-ADOrganizationalUnit -Filter "distinguishedName -eq '$DisabledUsersOU'") { Write-Host "$DisabledUsersOU already exists."} else { New-ADOrganizationalUnit -Name "Disabled Users" -Path $MainOU -ProtectedFromAccidentalDeletion $false}
+
+if ( Get-ADOrganizationalUnit -Filter "distinguishedName -eq '$DisabledComputersOU'") { Write-Host "$DisabledComputersOU already exists."} else { New-ADOrganizationalUnit -Name "Disabled Computers" -Path $MainOU -ProtectedFromAccidentalDeletion $false}
+
+###Searching for Users that meet criteria for disable, disabling, and moving to disabled OU###
+$DisabledUsers = Search-ADAccount -searchbase 'OU=IT,DC=Adatum,DC=Com' -AccountInactive -TimeSpan $InactivityDisable -UsersOnly
+foreach ($DisabledUser in $DisabledUsers){
+    $sam=$DisabledUser.samaccountname
+    $dn=$DisabledUser.distinguishedName
+
+    if ($dn -notmatch $DisabledUsersOU){
+        
+        $DisabledUser|Disable-ADAccount         
+         
+        $DisabledUser| Move-ADObject -TargetPath $DisabledUsersOU
+        Set-ADUser $sam -Description ("Moved from: " + $dn,"Due to Inactivity")
+    }
+}
+
+###Searching for Computers that meet criteria for disable, disabling, and moving to disabled OU###
+$DisabledComputers = Search-ADAccount -searchbase 'CN=Computers,DC=Adatum,DC=Com' -AccountInactive -TimeSpan $InactivityDisable -ComputersOnly
+foreach ($DisabledComputer in $DisabledComputers){
+    $sam=$DisabledComputer.samaccountname
+    $dn=$DisabledComputer.distinguishedName
+
+    if ($dn -notmatch $DisabledComputersOU){
+         
+        $DisabledComputer|Disable-ADAccount
+         
+        $DisabledComputer| Move-ADObject -TargetPath $DisabledComputersOU  
+        Set-ADComputer $sam -Description ("Moved from: " + $dn,"Due to Inactivity")
+    }
+} 
+
+###Searching for Users that meet criteria for deletion, copying that info to CSV file (CSV File is saved on the C:\) and then deleting the User account### 
+$Path = Split-Path -Parent "C:\*.*"
+$LogDate = Get-Date -f yyyyMMdd
+$Csvfile = $Path + "\AllADUsers_$logDate.csv"
+$DNs = $DisabledUsersOU
+
+$AllADUsers = @()
+
+foreach ($DN in $DNs) {
+    $Users = Get-ADUser -SearchBase $DN -Filter * -Properties * 
+
+        $AllADUsers += $Users
+}
+
+$AllADUsers | Sort-Object Name | Select-Object `
+@{Label = "First name"; Expression = { $_.GivenName } },
+@{Label = "Last name"; Expression = { $_.Surname } },
+@{Label = "Display name"; Expression = { $_.DisplayName } },
+@{Label = "User logon name"; Expression = { $_.SamAccountName } },
+@{Label = "User principal name"; Expression = { $_.UserPrincipalName } },
+@{Label = "Country/region"; Expression = { $_.Country } },
+@{Label = "Job Title"; Expression = { $_.Title } },
+@{Label = "Department"; Expression = { $_.Department } },
+@{Label = "Company"; Expression = { $_.Company } },
+@{Label = "Manager"; Expression = { % { (Get-AdUser $_.Manager -Properties DisplayName).DisplayName } } },
+@{Label = "Description"; Expression = { $_.Description } },
+@{Label = "Office"; Expression = { $_.Office } },
+@{Label = "Telephone number"; Expression = { $_.telephoneNumber } },
+@{Label = "E-mail"; Expression = { $_.Mail } },
+@{Label = "Account status"; Expression = { if (($_.Enabled -eq 'TRUE') ) { 'Enabled' } Else { 'Disabled' } } },
+@{Label = "Last logon date"; Expression = { $_.lastlogondate } }|
+
+Export-Csv -Encoding UTF8 -Path $Csvfile -NoTypeInformation #-Delimiter ";"
+
+$DeleteUsers = Search-ADAccount -searchbase $DisabledUsersOU -AccountInactive -TimeSpan $InactivityDelete -UsersOnly
+foreach ($DeleteUser in $DeleteUsers){
+    $sam=$DeleteUser.samaccountname
+    $dn=$DeleteUser.distinguishedName
+{
+        $DeleteUser|Remove-ADAccount -confirm $false
+        } }
+         
+###Searching for Computers that meet criteria for deletion, copying that info to CSV file (CSV File is saved on the C:\) for record and then deleting the Computer account###
+
+$Path = Split-Path -Parent "C:\*.*"
+$LogDate1 = Get-Date -f yyyyMMdd
+$Csvfile1 = $Path + "\AllADComputers_$logDate1.csv"
+$time = (Get-Date).Adddays(-($InactivityDelete))
+
+Get-ADComputer -SearchBase $DisabledComputersOU -Filter {LastLogonTimeStamp -lt $time} -ResultPageSize 1000 -resultSetSize $null -Properties Name, SamAccountName|
+Export-CSV -Encoding UTF8 –NoTypeInformation 
+
+$DeleteComputers = Search-ADAccount -searchbase 'CN=Computers,DC=Adatum,DC=Com' -AccountInactive -TimeSpan $InactivityDelete -ComputersOnly
+foreach ($DeleteComputer in $DeleteComputers){
+    $sam=$DeleteComputer.samaccountname
+    $dn=$DeleteComputer.distinguishedName
+
+{   $DeleteComputer|Remove-ADAccount -confirm $false  
+    }  }
