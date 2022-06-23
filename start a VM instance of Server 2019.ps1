04994fd6c5c317e7327004acbd752e520c427a69
JSnow38 <106977147+JSnow38@users.noreply.github.com>
2022-06-23 14:26:38 -0400

    Delete start a VM instance of Server 2019.ps1

--- start a VM instance of Server 2019.ps1
+++
@@ -1,8 +0,0 @@
-New-VMSwitch ExternalSwitch -NetAdapterName Ethernet -AllowManagementOS $true
-new-vm -name "SNO-DC1" -memorystartupbytes 2048mb -path "D:\school software"
-New-VHD -path "d:\school software\SNO-DC1.vhdx" -sizebytes 20gb -dynamic
-add-vmharddiskdrive -vmname "SNO-DC1" -path "d:\school software\SNO-DC1.vhdx"
-set-vmdvddrive -vmname "SNO-DC1" -path "D:\Windows OSs\Server 2019 trial version.iso"
-get-VMNetworkAdapter -VMName "SNO-DC1" | Connect-VMNetworkAdapter -Switchname ExternalSwitch 
-
-start-vm -name "SNO-DC1"
